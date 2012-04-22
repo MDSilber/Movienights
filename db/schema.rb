b05889afd2dd9c025936841bbd648d78e3576719
MDSilber <mds2161@columbia.edu>
2012-04-22 19:18:36 -0400

    Added a name column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120402022458) do
+ActiveRecord::Schema.define(:version => 20120422231521) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20120402022458) do
     t.string   "password_digest"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
+    t.string   "name"
   end
 
 end
