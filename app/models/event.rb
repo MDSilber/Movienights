d83f1fb2fd4e704938199443f693b619dea04e23
MDSilber <mds2161@columbia.edu>
2012-05-06 17:50:24 -0400

    Top three ranked movies now show up on the event show page

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,4 +9,32 @@ class Event < ActiveRecord::Base
   has_many :movies, :through => :suggestions
   has_many :users, :through => :invitations
 
+
+  def get_top_three
+    movies = Hash.new
+    
+    self.suggestions.each do |s|
+      s.rankings.each do |r|
+        if movies[s.movie.title]
+          movies[s.movie.title] += r.value
+        else
+          movies[s.movie.title] = r.value
+        end
+      end
+    end
+    
+    movies.sort_by{ |title,value| value}
+
+    top_three = Array.new
+    
+    counter = 0
+    movies.each do |key, value|
+      top_three[counter] = key
+      counter += 1
+      break if counter == 3
+    end
+    
+    top_three
+  end
+
 end
